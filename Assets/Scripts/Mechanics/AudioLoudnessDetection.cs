6f61f74238376c0b5ff2ac55f271dafeb5c635e9
CakuDev <cakudev@gmail.com>
2024-08-17 15:04:30 +0200

    Voice feature

--- Assets/Scripts/Mechanics/AudioLoudnessDetection.cs
+++ Assets/Scripts/Mechanics/AudioLoudnessDetection.cs
@@ -1,59 +1,100 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
-public class AudioLoudnessDetection : MonoBehaviour
+namespace UnityWebGLMicrophone
 {
-    // Start is called before the first frame update
-#if UNITY_EDITOR
-    public int sampleWindow = 64;
-    private AudioClip microphoneClip;
-
-    void Start()
+    public class AudioLoudnessDetection : MonoBehaviour
     {
-        MicrophoneToAudioClip();
-    }
+        // Start is called before the first frame update
+        public int sampleWindow = 64;
+        private AudioClip microphoneClip;
+        private int microphoneIndex;
+        public float minScaleThreshold = 0.001f;
+        public float scaleThreshold = 0.1f;
+        public float scaleVelocity = 10f;
+        public TextMeshProUGUI microphoneVolume;
 
-    // Update is called once per frame
-    void Update()
+#if UNITY_WEBGL && !UNITY_EDITOR
+    void Awake()
     {
-        
-    }
-
-    public void MicrophoneToAudioClip()
-    {
-        //First microphone in device list
-        string microphoneName = Microphone.devices[0];
-        Debug.Log(microphoneName);
-        microphoneClip = Microphone.Start(microphoneName, true, 20, AudioSettings.outputSampleRate);
-    }
-
-    public float GetLoudnessFromMicrophone(int microphoneIndex)
-    {
-        return GetLoudnessFromAudioClips(Microphone.GetPosition(Microphone.devices[microphoneIndex]), microphoneClip);
-    }
-
-    public float GetLoudnessFromAudioClips(int clipPosition, AudioClip clip)
-    {
-        int startPosition = clipPosition - sampleWindow;
-
-        if (startPosition < 0)
-        {
-            return 0;
-        }
-
-        float[] waveData = new float[sampleWindow];
-        clip.GetData(waveData, startPosition);
-
-        //compute loudness
-        float totalLoudness = 0;
-
-        for (int i = 0; i < sampleWindow; i++)
-        {
-            totalLoudness += Mathf.Abs(waveData[i]);
-        }
-
-        return totalLoudness / sampleWindow;
+        Microphone.Init();
+        Microphone.QueryAudioInput();
     }
 #endif
+
+        void Start()
+        {
+#if !UNITY_WEBGL
+            MicrophoneToAudioClip();
+#endif
+        }
+
+        public float GetScaleChangeVariation()
+        {
+            float loudness;
+#if UNITY_WEBGL && !UNITY_EDITOR
+        Microphone.Update();
+        float[] volumes = Microphone.volumes;
+            
+        loudness = volumes[microphoneIndex];
+#endif
+#if !UNITY_WEBGL || UNITY_EDITOR
+            loudness = GetLoudnessFromMicrophone() * 10;
+#endif
+
+            microphoneVolume.text = loudness.ToString();
+            if (loudness < minScaleThreshold) return 0f;
+            if (loudness > scaleThreshold)
+            {
+                return scaleVelocity * Time.deltaTime;
+            }
+            else
+            {
+                return -scaleVelocity * Time.deltaTime;
+            }
+        }
+#if !UNITY_WEBGL || UNITY_EDITOR
+        public void MicrophoneToAudioClip()
+        {
+            //First microphone in device list
+            string microphoneName = Microphone.devices[0];
+            microphoneClip = Microphone.Start(microphoneName, true, 20, AudioSettings.outputSampleRate);
+        }
+
+        public float GetLoudnessFromMicrophone()
+        {
+            return GetLoudnessFromAudioClips(Microphone.GetPosition(Microphone.devices[microphoneIndex]), microphoneClip);
+        }
+
+        public void ChangeMicrophoneIndex(int index)
+        {
+            microphoneIndex = index;
+        }
+
+        public float GetLoudnessFromAudioClips(int clipPosition, AudioClip clip)
+        {
+            int startPosition = clipPosition - sampleWindow;
+
+            if (startPosition < 0)
+            {
+                return 0;
+            }
+
+            float[] waveData = new float[sampleWindow];
+            clip.GetData(waveData, startPosition);
+
+            //compute loudness
+            float totalLoudness = 0;
+
+            for (int i = 0; i < sampleWindow; i++)
+            {
+                totalLoudness += Mathf.Abs(waveData[i]);
+            }
+
+            return totalLoudness / sampleWindow;
+        }
+#endif
+    }
 }
--- Assets/Scripts/Mechanics/ScaleFromWebMicrophone.cs
+++ Assets/Scripts/Mechanics/ScaleFromWebMicrophone.cs
@@ -4,69 +4,21 @@ using UnityEngine;
 namespace UnityWebGLMicrophone
 {
     public class ScaleFromWebMicrophone : MonoBehaviour
-    {
-
-        public float loudnessSensitibility = 100;
-        public float loudnessThreshold = 0.1f;
-        public float scaleThreshold = 10;
-        public float scaleVelocity = 10;
-
-        public float minScale = 0.5f;
-        public float maxScale = 100f;
-        
-        public TextMeshProUGUI microphoneVolume;
-
-        private int microphoneIndex = 0;
-
-#if UNITY_EDITOR
-
+    {    
         public AudioLoudnessDetection detector;
-
-#endif
-
-#if UNITY_WEBGL && !UNITY_EDITOR
-        void Awake()
-        {
-            Microphone.Init();
-            Microphone.QueryAudioInput();
-        }
-#endif
+        public float maxScale = 10f;
+        public float minScale = 0.5f;
 
         void Update()
         {
-            string[] devices = Microphone.devices;
+            float scaleVariation = detector.GetScaleChangeVariation();
+            if (scaleVariation == 0 || !Input.GetMouseButton(1)) return;
+            Vector3 newScale = transform.localScale + new Vector3(scaleVariation, scaleVariation, scaleVariation);
+            if (scaleVariation > 0 && transform.localScale.x > maxScale) newScale = new(maxScale, maxScale, maxScale);
+            if (scaleVariation < 0 && transform.localScale.x < minScale) newScale = new(minScale, minScale, minScale);
 
-            float loudness = GetMicrophoneLoudness();
-            microphoneVolume.text = loudness.ToString();
-            if (loudness < 0.1f) return;
-            if (loudness > scaleThreshold)
-            {
-                if (transform.localScale.x < maxScale) transform.localScale = transform.localScale + (new Vector3(scaleVelocity, scaleVelocity, scaleVelocity) * Time.deltaTime * loudness);
-            } else
-            {
-                if (transform.localScale.x > minScale) transform.localScale = transform.localScale - (new Vector3(scaleVelocity, scaleVelocity, scaleVelocity) * Time.deltaTime * (1 - loudness));
-            }
-
-        }
-
-        private float GetMicrophoneLoudness()
-        {
-            float loudness = 0;
-#if UNITY_WEBGL && !UNITY_EDITOR
-            Microphone.Update();
-            float[] volumes = Microphone.volumes;
-            
-            loudness = volumes[microphoneIndex];
-#endif
-#if !UNITY_WEBGL || UNITY_EDITOR
-            loudness = detector.GetLoudnessFromMicrophone(microphoneIndex) * 10;
-#endif
-            return loudness;
-        }
-
-        public void changeMicrophoneIndex(int index)
-        {
-            microphoneIndex = index;
+            Debug.Log(newScale);
+            transform.localScale = newScale;
         }
     }
 }
