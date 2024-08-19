329685383ec2978bb7affc779dd5a80eb659952b
Isabel Arrans Vega <67335367+iarrans@users.noreply.github.com>
2024-08-19 20:17:44 +0200

    UI and tests music

--- Assets/Scripts/UI/AudioManager.cs
+++ Assets/Scripts/UI/AudioManager.cs
@@ -5,15 +5,24 @@ using static UnityEngine.Rendering.DebugUI;
 
 public class AudioManager : MonoBehaviour
 {
+    public AudioSource BGMSource;
+
+    public AudioSource MagicSFXSource;
+
+    public AudioSource SFXSource;
+
+    public AudioManager instance;
+    
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("Audio" + AudioListener.volume);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PlayAudioClip(AudioClip audioclip)
     {
-        
+        SFXSource.clip = audioclip;
+        SFXSource.Play();
     }
+   
 }
